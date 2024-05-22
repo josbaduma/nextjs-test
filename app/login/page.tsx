5e6a0d763fd76099550c2239dbbbc1ac9ff1e42a
Jose Badilla <josebaduma@gmail.com>
2024-05-22 10:48:28 -0600

    Minor Fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'LogIn',
+  title: 'Log In',
 };
 
 export default function LoginPage() {
